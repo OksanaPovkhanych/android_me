8e4db7eae9f295365fdbff01cfb5d128bafa9a94
Oksana Povkhanych <oknevskaya@gmail.com>
2020-03-16 20:02:00 +0200

    Final commit

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -4,24 +4,80 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.android.android_me.R;
-import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BodyPartFragment extends Fragment {
+    private static final String IMAGE_ID_LIST = "image_ids";;
+    private static final String LIST_INDEX = "list_index";
+
+    private int mListIndex;
+    private List<Integer> mImageIds;
+
+    public void setListIndex(int mListIndex) {
+        this.mListIndex = mListIndex;
+    }
+
+    public void setImageIds(List<Integer> mImageIds) {
+        this.mImageIds = mImageIds;
+    }
+
     public BodyPartFragment() {
     }
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_body_part,container, false);
-        ImageView imageView =  (ImageView) rootView.findViewById(R.id.body_part_image_view);
-        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+        // Load the saved state (the list of images and list index) if there is one
+        if(savedInstanceState != null) {
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
+        }
+
+        // Inflate the Android-Me fragment layout
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        // Get a reference to the ImageView in the fragment layout
+        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        // If a list of image ids exists, set the image resource to the correct item in that list
+        // Otherwise, create a Log statement that indicates that the list was not found
+        if(mImageIds != null){
+            // Set the image resource to the list item at the stored index
+            imageView.setImageResource(mImageIds.get(mListIndex));
+
+            // Set a click listener on the image view
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Increment position as long as the index remains <= the size of the image ids list
+                    if(mListIndex < mImageIds.size()-1) {
+                        mListIndex++;
+                    } else {
+                        // The end of list has been reached, so return to beginning index
+                        mListIndex = 0;
+                    }
+                    // Set the image resource to the new list item
+                    imageView.setImageResource(mImageIds.get(mListIndex));
+                }
+            });
+
+        } else {
+            Log.e("BodyPartFragment","Empty list of images in fragment");
+        }
         return rootView;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle currentState) {
+        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
+        currentState.putInt(LIST_INDEX, mListIndex);
     }
 }
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
 public class MasterListFragment extends Fragment {
+    OnImageClickListener mCallback;
+
+    public  interface  OnImageClickListener {
+        void OnImageSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mCallback = (OnImageClickListener) context;
+        }
+        catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement OnImageSelected ");
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,6 +47,12 @@ public class MasterListFragment extends Fragment {
 
         // Set the adapter on the GridView
         gridView.setAdapter(mAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCallback.OnImageSelected(position);
+            }
+        });
         return  rootView;
     }
 }
